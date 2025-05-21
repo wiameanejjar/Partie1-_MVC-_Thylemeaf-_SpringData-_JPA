1bc5b15f5fa3ee7de0ff0ffe345c0b27555062b2
wiameanejjar <wiameanejjar@gmail.com>
2025-05-21 18:02:15 +0100

    troisième commit

--- src/main/java/ma/fs/hopital/HopitalApplication.java
+++ src/main/java/ma/fs/hopital/HopitalApplication.java
@@ -1,13 +1,46 @@
 package ma.fs.hopital;
 
+import ma.fs.hopital.entities.Patient;
+import ma.fs.hopital.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Date;
+
 @SpringBootApplication
-public class HopitalApplication {
+public class HopitalApplication implements CommandLineRunner {
+	@Autowired
+	private PatientRepository patientRepository;
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(HopitalApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		patientRepository.save(new Patient(null,"Mohamed",new Date(),false, 34));
+		patientRepository.save(new Patient(null,"Hanane",new Date(),false, 3421));
+		patientRepository.save(new Patient(null,"Imane",new Date(),true, 34));
+		/*Patient patient = new Patient();
+		patient.setId(null);
+		patient.setNom("Mohamed");
+		patient.setDateNaissance(new Date());
+		patient.setMalade(false);
+		patient.setScore(23);*/
+
+		//Patient patient2 = new Patient(null, "Yassine",new Date(), false,123);
+        // En utilisant Builder
+		/*Patient patient3 = Patient.builder()
+				.nom("Imane")
+				.dateNaissance(new Date())
+				.score(56)
+				.malade(true)
+				.build();*/
+
+
+
+	}
 }
--- src/main/java/ma/fs/hopital/web/PatientController.java
+++ src/main/java/ma/fs/hopital/web/PatientController.java
@@ -1,4 +1,46 @@
 package ma.fs.hopital.web;
 
+
+import lombok.AllArgsConstructor;
+import ma.fs.hopital.entities.Patient;
+import ma.fs.hopital.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@AllArgsConstructor
 public class PatientController {
+
+    private PatientRepository patientRepository;
+    @GetMapping("/index")
+    public String index(Model model, @RequestParam(name = "page", defaultValue = "0") int p,
+                        @RequestParam(name = "size", defaultValue = "4") int s,
+                        @RequestParam(name = "keyword", defaultValue = "") String kw
+                        ) {
+        //Page<Patient> pagePatients = patientRepository.findAll(PageRequest.of(p,s));
+        Page<Patient> pagePatients = patientRepository.findByNomContains(kw, PageRequest.of(p, s));
+
+        model.addAttribute("listPatients", pagePatients.getContent());
+        model.addAttribute("pages", new int[pagePatients.getTotalPages()]);
+        model.addAttribute("currentPage", p);
+        model.addAttribute("keyword", kw);
+        return "patients";
+    }
+    @GetMapping("/delete")
+    public String delete(@RequestParam(name = "id") Long id, @RequestParam(name = "keyword", defaultValue = "") String keyword,
+                         @RequestParam(name = "page", defaultValue = "0") int page) {
+        patientRepository.deleteById(id);
+        return "redirect:/index?page=" + page + "&keyword=" + keyword;
+    }
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/index";
+    }
 }
